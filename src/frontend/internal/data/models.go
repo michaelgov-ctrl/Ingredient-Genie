c5448926f91a07d9ed24f3ebf30ebd31c968fbbb
Michael Governanti <michaelgovernanti@gmail.com>
2026-07-21 08:54:09 -0400

    Scaffold Frontend
    This commit begins to scaffold out the frontend API. It laysout
    the http server and model for interacting with the backend API.

---
+++ src/frontend/internal/data/models.go
@@ -0,0 +1,50 @@
+package data
+
+import (
+	"log/slog"
+	"net"
+)
+
+type MealsApi interface {
+	SearchByIngredients() (MealResponse, Metadata, error)
+}
+
+type Models struct {
+	MealsApi MealsApi
+}
+
+func NewModels(logger *slog.Logger, mealApiAddr net.Addr) Models {
+	return Models{
+		MealsApi: NewMealsApiClient(logger, mealApiAddr),
+	}
+}
+
+type MealResponse struct {
+	Meal                   Meal    `json:"meal"`
+	MatchedIngredientCount int64   `json:"matchedIngredientCount"`
+	TotalIngredientCount   int64   `json:"totalIngredientCount"`
+	MissingIngredientCount int64   `json:"missingIngredientCount"`
+	MatchRatio             float64 `json:"matchRatio"`
+}
+
+type Meal struct {
+	ID            int64  `json:"id"`
+	MealDBID      int64  `json:"mealDbId"`
+	Name          string `json:"name"`
+	AlternateName string `json:"alternateName"`
+	Category      string `json:"category"`
+	Area          string `json:"area"`
+	Country       string `json:"country"`
+	Instructions  string `json:"instructions"`
+	ThumbnailUrl  string `json:"thumbnailUrl"`
+	YoutubeUrl    string `json:"youtubeUrl"`
+	SourceUrl     string `json:"sourceUrl"`
+}
+
+type Metadata struct {
+	CurrentPage  int `json:"current_page"`
+	PageSize     int `json:"page_size"`
+	FirstPage    int `json:"first_page"`
+	LastPage     int `json:"last_page"`
+	TotalRecords int `json:"total_records"`
+}
